841c10bf066d48ed6c3b2e717e0338a7aa89574e
JStanl3y <98609202+JStanl3y@users.noreply.github.com>
2022-04-14 15:34:21 -0400

    Add files via upload

---
+++ Airport.cpp
@@ -0,0 +1,64 @@
+#include Nav_Aids
+#include Airport.h
+#include string
+#include vector
+
+using namespace std;
+
+
+
+
+	bool Airport::Degree_Clearance(int planeCoords[], string runway) 
+	{
+		int i = Identify_Runway(runway);
+		refAngle = calculated degrees between planeCoords and runwayEndPoints[i] // cos^-1(a/h) 
+		if(refAngle <= 30)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+		
+	bool Airport::Runway_Availability(string runway)
+	{
+		int i = Identify_Runway(runway);
+		return runwayAvailable[i];
+
+	}
+	
+	void Airport::Empty_Runway(string runway)
+	{
+		int i = Identify_Runway(runway);
+		runwayAvailable[i] = true;
+
+		
+	}
+	
+	void Airport::Occupy_Runway(string runway)
+	{
+		int i = Identify_Runway(runway);
+		runwayAvailable[i] = false;
+
+	}
+	
+	Nav_Aids Airport::Get_Runway(string runway)
+	{
+		int i = Identify_Runway(runway);
+		return runwayEndPoints[i];
+
+	}
+	
+	int Airport::Identify_Runway(string runway)
+	{
+		for(int i = 0; i<runwayEndPoints.length;i++)
+		{
+			if(runway == runwayEndPoints[i].ID)
+			{
+				return i;
+			}
+		}
+		//if invalid runway name what should we do in this case? or is it already checked
+	}
---
+++ Airport.h
@@ -0,0 +1,18 @@
+#include Nav_Aids
+#include Airport.cpp
+#include string
+#include vector
+
+using namespace std;
+
+public class Airport {
+	private:
+		const vector<Nav_Aids> runwayEndPoints; // contains name and coordinate locations of each runway
+		bool runwayAvailable[4]; // true means the runway is unavailable and false means it is available
+	public:
+		bool Airport::Degree_Clearance(int heading, string runway);
+		bool Airport::Runway_Availability(string runway);
+		Nav_Aids Airport::Get_Runway(string runway);
+		void Airport::Empty_Runway(string runway);
+		void Airport::Occupy_Runway(string runway);
+}
