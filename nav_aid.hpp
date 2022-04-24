f4513111a45905ec33bf9979b3a2a8377a1e3eaf
Conor Rybacki <rybackiconor@gmail.com>
2022-04-24 19:49:07 -0400

    updated with new format

--- nav_aid.hpp
+++ nav_aid.hpp
@@ -3,8 +3,22 @@
 
 #include <string>
 
-struct nav_aid{
+struct nav_aid
+{
 	string ID;
 	int coordinates[2];
 };
 #endif
+
+vector<nav_aid> getNavAids()
+{
+	vector<nav_aid> local;
+
+	nav_aid navA;
+	navA.ID = "daisy";
+	navA.coordinates[0] = 30000; // x
+	navA.coordinates[1] = 12000; // y
+	local.push_back(navA);
+
+	return local;
+}
