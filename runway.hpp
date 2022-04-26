96b82ae29e010c018fe5ca2004929b9a2b5dcbf4
Conor Rybacki <rybackiconor@gmail.com>
2022-04-26 19:28:45 -0400

    fixed error in runway

--- runway.hpp
+++ runway.hpp
@@ -11,7 +11,7 @@ struct runway_ends
 	string runwayID;
 	vector<int> coordinates;
 	int heading = 0;
-	bool available = false;			   // is it available
+	bool available = false; // is it available
 };
 
 vector<runway_ends> runwayInfo()
@@ -26,16 +26,15 @@ vector<runway_ends> runwayInfo()
 	topLeft.runwayID = "9L";
 	topRight.runwayID = "27R";
 
-	topLeft.coordinates.push_back(-1950);  // x
-	topLeft.coordinates.push_back(685);    // y
-	topLeft.coordinates.push_back(0);	// z
+	topLeft.coordinates.push_back(-1950); // x
+	topLeft.coordinates.push_back(685);	  // y
+	topLeft.coordinates.push_back(0);	  // z
 
-	topRight.coordinates.push_back(1950);	// x
-	topRight.coordinates.push_back(685);		// y
-	topRight.coordinates.push_back(0);		// z
+	topRight.coordinates.push_back(1950); // x
+	topRight.coordinates.push_back(685);  // y
+	topRight.coordinates.push_back(0);	  // z
 
-
-	topLeft.heading = 270;				// base heading around the center coordinate
+	topLeft.heading = 270; // base heading around the center coordinate
 	topRight.heading = 90;
 	topLeft.available = true;
 	topRight.available = topLeft.available;
@@ -43,18 +42,17 @@ vector<runway_ends> runwayInfo()
 	botLeft.runwayID = "9R";
 	botRight.runwayID = "27L";
 	botLeft.coordinates.push_back(-1709); // x
-	botLeft.coordinates.push_back(-685);	// y
-	botLeft.coordinates.push_back(0);	// z
+	botLeft.coordinates.push_back(-685);  // y
+	botLeft.coordinates.push_back(0);	  // z
 
-	botLeft.coordinates.push_back(1950);	// x
-	botRight.coordinates.push_back(-685);	// y
-	botRight.coordinates.push_back(0);	// z
+	botRight.coordinates.push_back(1950); // x
+	botRight.coordinates.push_back(-685); // y
+	botRight.coordinates.push_back(0);	  // z
 
-	botLeft.heading = 270;				// base heading around the center coordinate
+	botLeft.heading = 270; // base heading around the center coordinate
 	botRight.heading = 90;
 	botRight.available = true;
 	botLeft.available = botRight.available;
-
 
 	local.push_back(topLeft);
 	local.push_back(botLeft);
