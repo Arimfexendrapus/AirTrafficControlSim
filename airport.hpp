27301a31d41b7a0c363a30136c6ebc9615ec6fc4
Conor Rybacki <rybackiconor@gmail.com>
2022-04-26 18:45:59 -0400

    working airport

--- airport.hpp
+++ airport.hpp
@@ -8,7 +8,7 @@
 #include <math.h>
 #include "nav_aid.hpp"
 #include "runway.hpp"
-#include "airplane.hpp"
+
 
 using namespace std;
 
@@ -18,30 +18,29 @@ private:
 	vector<runway_ends> runways; // contains runways
 public:
 	airport(); // constructor
-	bool degree_clearance(string runwayID, airplane *Plane);
+	bool degree_clearance(string runwayID, vector<int> coords, int aircraftHeading);
 	bool runway_availability(string runwayID);
-	runway_ends* get_runway(string runwayID);
+	runway_ends *get_runway(string runwayID);
 	void empty_runway(string runwayID);
 	void occupy_runway(string runwayID);
 	int identify_runway(string runwayID);
 };
 
-
-bool airport::degree_clearance(string runwayID, airplane *Plane)
+bool airport::degree_clearance(string runwayID, vector<int> coords, int aircraftHeading)
 {
 	int i = identify_runway(runwayID);
-	vector<int> planeCoords = Plane->get_coordinates();
+	vector<int> planeCoords = coords;
 
 	int refAngle =
 		abs((180 / M_PI) * acos((planeCoords[0] - runways[i].coordinates[0]) // divided by
-			/ ((planeCoords[0] - runways[i].coordinates[0]) ^ 2 +
-				(planeCoords[1] - runways[i].coordinates[1]) ^ 2) ^ (1 / 2))); // cos^-1(a/h)
+									/ ((planeCoords[0] - runways[i].coordinates[0]) ^ 2 + (planeCoords[1] - runways[i].coordinates[1]) ^ 2) ^
+								(1 / 2))); // cos^-1(a/h)
 
 	if (refAngle <= 30)
 	{
-		if (Plane->get_heading() > runways[i].heading - 30)
+		if (aircraftHeading > runways[i].heading - 30)
 		{
-			if (Plane->get_heading() < runways[i].heading + 30)
+			if (aircraftHeading < runways[i].heading + 30)
 			{
 				return true;
 			}
@@ -59,14 +58,13 @@ int airport::identify_runway(string runwayID)
 {
 	for (int i = 0; i < runways.size(); i++)
 	{
-		if (runwayID == runways[i].runwayID) return i;
+		if (runwayID == runways[i].runwayID)
+			return i;
 	}
 
 	return -1;
 	// if invalid runway name what should we do in this case? or is it already checked
 }
-
-
 
 bool airport::runway_availability(string runwayID)
 {
@@ -86,7 +84,7 @@ void airport::occupy_runway(string runway)
 	runways[i].available = false;
 }
 
-runway_ends* airport::get_runway(string runway)
+runway_ends *airport::get_runway(string runway)
 {
 	int i = identify_runway(runway);
 	return &runways[i];
